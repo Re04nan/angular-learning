6013ab48e8130affea2f3ae443859e1b571f4213
Re04nan <renan04_marques@hotmail.com>
2021-10-10 14:39:08 -0300

    Adicionando pratica com event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,13 +3,23 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.scss']
+  //styleUrls: ['./data-binding.component.scss']
+  styles: [
+    `.highlight{
+      background-color: yellow;
+      font-weight: bold;
+      }
+    `
+  ]
 })
 export class DataBindingComponent implements OnInit {
 
   name :string = 'Renan Marques';
   courseAngular: boolean = true;
   urlImage: string = 'http://lorempixel.com/g/400/200/';
+  valueActual: string = '';
+  valueSave: string = '';
+  isMouseOver: boolean = false;
 
   getValue(){
     return 1;
@@ -19,6 +29,18 @@ export class DataBindingComponent implements OnInit {
     return true;
   }
 
+  btnClick(){
+    return alert("Clicouuu");
+  }
+  onKeyUp(event: KeyboardEvent){
+    this.valueActual=(<HTMLInputElement>event.target).value;
+  }
+  saveValue(value: string){
+    this.valueSave = value;
+  }
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
   constructor() { }
 
   ngOnInit(): void {
